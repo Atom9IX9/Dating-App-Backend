a421ade083802a0bc380877ef5fd67dc95e04b2c
Yaroslav <kprogrammer0pr@gmail.com>
2024-09-27 15:17:48 +0300

    Reset with Nest.js

--- src/types/requests.ts
+++
@@ -1,5 +0,0 @@
-import { Request } from "express";
-
-export type BodyRequest<T> = Request<{}, {}, T>
-export type QueryRequest<T> = Request<{}, {}, {}, T>
-export type ParamsRequest<T> = Request<T>
