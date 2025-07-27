efd1472b650cc5f94e5beba459ef1616f2f89db5
Atom9IX9 <vorobjovaroslav33@gmail.com>
2025-07-27 22:52:16 +0300

    Docker: - Dockerfile - docker-compose.yml: -- Migration to PostreSQL with PgAdmin in Docker

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,11 +1,13 @@
 export default () => ({
   port: process.env.PORT,
-  dbPort: process.env.DB_PORT,
-  dbHost: process.env.DB_HOST,
-  dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dbName: process.env.DB_NAME,
   jwtSecret: process.env.JWT_SECRET,
   jwtExpire: process.env.JWT_EXPIRE,
   jwtRememberMeExpire: process.env.JWT_REMEMBER_ME_EXPIRE,
+  db: {
+    port: +process.env.POSTGRES_PORT,
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    name: process.env.POSTGRES_DB,
+  },
 });
