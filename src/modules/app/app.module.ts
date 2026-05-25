9a0a4fc2f5b20b65f98977e7d24993fce56e4e5a
Atom9IX9 <vorobjovaroslav33@gmail.com>
2026-05-26 00:24:29 +0300

    fix: set Sequelize synchronize option to true for development environment

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -37,7 +37,7 @@ import { join } from 'path';
         username: configService.get('db.user'),
         password: configService.get('db.password'),
         database: configService.get('db.name'),
-        synchronize: process.env.NODE_ENV !== 'production', //after development, use migrations instead of sync / on dev - true to auto create tables based on models, on prod - false to avoid data loss
+        synchronize: true, //after development, use migrations instead of sync / on dev - true to auto create tables based on models, on prod - false to avoid data loss
         //sync: { force: true }, //force: true - drop tables and recreate them on every app restart (use only for development)
         autoLoadModels: true,
       }),
